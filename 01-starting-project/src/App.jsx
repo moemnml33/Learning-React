700b332a85d0312f98ae83228d93fafa7170ed3f
moe booka <mhmdbkaili@hotmail.com>
2024-05-16 14:07:21 -0400

    Update App.jsx
    output components dynamically rather than explicitly/statically

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -52,16 +52,23 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
-            />
+            /> */}
             {/* spread operator: built in javascript to pull out that's built-in to JavaScript to pull out 
               all the key value pairs of an object */}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+
+            {/* dynamically outputting components: */}
+            {/* transform the javascript array of objects to an array of JSX elements
+            map each item of the javascript list of objects to a component */}
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
--- 01-starting-project/src/data.js
+++ 01-starting-project/src/data.js
@@ -3,6 +3,7 @@ import propsImg from "./assets/config.png";
 import jsxImg from "./assets/jsx-ui.png";
 import stateImg from "./assets/state-mgmt.png";
 
+// array of javascript objects
 export const CORE_CONCEPTS = [
   {
     image: componentsImg,
